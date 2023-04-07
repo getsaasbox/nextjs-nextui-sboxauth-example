8cd5cf6b95f1ae47e2ae880036b17b61d34e110d
Team SaaSBox <team@saasbox.net>
2023-04-06 21:52:28 -0700

    Add heroicons

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@
 import withSboxAuth from 'sbox-auth-next';
 import { AppCard } from '../components/AppCard'
 import { Container, Card, Row, Text } from "@nextui-org/react";
+import { Card as TremorCard, 
+         Text as TremorText,
+         Metric as TremorMetric } from "@tremor/react";
 
 function Home({ user }) {
   return (
     <Container>
     <AppCard user={user} />
+    <TremorCard>
+      <TremorText>Sales</TremorText>
+      <TremorMetric>$ 34,743</TremorMetric>
+    </TremorCard>
     </Container>
   );
 }
