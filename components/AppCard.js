fceb8cb1fd3c7a0b973a9a4608359020450e0d7d
Team SaaSBox <team@saasbox.net>
2023-04-10 22:38:12 -0700

    Display plan information as well

--- components/AppCard.js
+++ components/AppCard.js
@@ -2,6 +2,21 @@ import { Avatar, Spacer, Card, Grid, Text, Link, Row } from "@nextui-org/react";
 
 export function AppCard({user}) {
   const username = user.firstname + " " + user.lastname;
+  let status;
+  let plan = null;
+
+  if (user.is_admin) {
+    status = "an administrator"
+  } else {
+    if (user.is_customer) {
+      status = "a customer"
+    } else {
+      status = "a regular user";
+    }
+    if (user.plan) {
+      plan = user.plan.name;
+    }
+  }
   return (<>
     <Card css={{ p: "$6", mw: "400px" }}>
       <Card.Header>
@@ -18,9 +33,11 @@ export function AppCard({user}) {
         </Grid.Container>
       </Card.Header>
       <Card.Body css={{ py: "$2" }}>
-        <Text>
-          This user is {user.is_admin ? "an administrator" : "a regular user." }
-        </Text>
+        <Row>
+          <Text>
+            This user is {status} {plan ? "on a \"" + plan + "\" plan." : "." }
+          </Text>
+        </Row>
       </Card.Body>
       <Card.Footer>
         <Row>
